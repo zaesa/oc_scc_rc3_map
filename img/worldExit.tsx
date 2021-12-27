c396177b8731557bbec76285702008dd436cee62
zaesa <zaesa@posteo.eu>
2021-12-27 12:05:32 +0100

    added tilesets as png

--- img/worldExit.tsx
+++ img/worldExit.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="worldExit" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="world-tiles/rc3-2020/structures/worldExit.png" width="64" height="64"/>
+ <image source="../../../../Downloads/worldExit.png" width="64" height="64"/>
 </tileset>
