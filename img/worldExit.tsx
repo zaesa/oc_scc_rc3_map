83c42c969a03f3e744f732a46cc5f16dd17da00e
zaesa <zaesa@posteo.eu>
2021-12-27 12:57:09 +0100

    fixed png paths, added start tile

--- img/worldExit.tsx
+++ img/worldExit.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="worldExit" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../../../Downloads/worldExit.png" width="64" height="64"/>
+ <image source="../img_png/worldExit.png" width="64" height="64"/>
 </tileset>
